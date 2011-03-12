79a3c63ef3d0933c1a1168a86ad6a35969aa7e58
Rémi Attab <attab.remi@gmail.com>
2011-03-12 10:20:43 -0500

    Implemented folder simplification except copy.

--- src/filevents.cpp
+++ src/filevents.cpp
@@ -14,6 +14,7 @@
 #include <map>
 #include <set>
 #include <algorithm>
+#include <iterator>
 
 #include <cstdlib>
 #include <cstdio>
@@ -23,6 +24,7 @@
 
 static const long TIMESTAMP_TOLERANCE = 100;
 static const std::string SEP("/");
+static const std::string NULL_HASH("-");
 
 
 class t_folder;
@@ -53,6 +55,20 @@ typedef t_hash_index::value_type t_index_pair;
 typedef t_hash_index::iterator t_index_it;
 typedef t_hash_index::const_iterator t_index_cit;
 
+// Maps a sub-path to a hash value.
+typedef std::map<std::string, t_hash> t_tree;
+typedef t_tree::value_type t_tree_pair;
+typedef t_tree::iterator t_tree_it;
+typedef t_tree::const_iterator t_tree_cit;
+
+// Maps folder names to their tree.
+typedef std::multimap<std::string, t_tree> t_tree_index;
+typedef t_tree_index::value_type t_tree_index_pair;
+typedef t_tree_index::iterator t_tree_index_it;
+typedef t_tree_index::const_iterator t_tree_index_cit;
+
+
+
 
 template <typename Iterator>
 Iterator dec(const Iterator& it) {
@@ -70,17 +86,21 @@ Iterator inc(const Iterator& it) {
 
 
 
-std::string path_to_string (const t_path& path) {
+
+std::string path_to_string (const t_path_cit start, const t_path_cit end) {
   std::stringstream ss;
 
-  for(t_path_cit it = path.begin(); it != path.end(); ++it) {
+  for(t_path_cit it = start; it != end; ++it) {
     ss << *it;
-    if (*it != SEP && inc(it) != path.end())
+    if (*it != SEP && inc(it) != end)
       ss << SEP;
   }
   return ss.str();
 }
 
+std::string path_to_string (const t_path& path) {
+  return path_to_string(path.begin(), path.end());
+}
 
 t_path make_path (const std::string& raw_path) {
   t_path path;
@@ -114,11 +134,32 @@ t_path get_parent (const t_path& path) {
   return t_path(path.begin(), dec(path.end()));
 }
 
+bool operator== (const t_path& lhs, const t_path& rhs) {
+  if (lhs.size() != rhs.size()) 
+    return false;
+
+  t_path_cit lhs_it = lhs.begin();
+  t_path_cit rhs_it = rhs.begin();
+
+  for (; lhs_it != lhs.end(); ++lhs_it, ++rhs_it) {
+    if (*lhs_it != *rhs_it)
+      return false;
+  }
+  return true;
+}
 
 
 
 
-enum t_event_type {
+typedef std::map<std::string, t_hash> t_tree;
+typedef t_tree::value_type t_tree_pair;
+typedef t_tree::iterator t_tree_it;
+typedef t_tree::const_iterator t_tree_cit;
+
+
+
+
+enum t_event_type { 
   e_new,
   e_delete,
   e_modify,
@@ -159,9 +200,24 @@ struct t_event_set_comp : public std::binary_function<p_event, p_event, bool> {
   }
 };
 
-typedef std::set<p_event, t_event_set_comp> t_event_list;
+typedef std::multiset<p_event, t_event_set_comp> t_event_list;
 typedef t_event_list::iterator t_event_it;
 typedef t_event_list::const_iterator t_event_cit;
+
+
+bool operator== (const t_event_list& lhs, const t_event_list& rhs) {
+  if (lhs.size() != rhs.size()) 
+    return false;
+
+  t_event_cit lhs_it = lhs.begin();
+  t_event_cit rhs_it = rhs.begin();
+
+  for (; lhs_it != lhs.end(); ++lhs_it, ++rhs_it) {
+    if (*lhs_it != *rhs_it)
+      return false;
+  }
+  return true;  
+}
 
 
 
@@ -170,7 +226,7 @@ struct t_event_new : public t_event {
   t_path path;
   t_hash hash;
 
-  t_event_new(t_file_type f, long ts, const t_path& p, t_hash h = "") : 
+  t_event_new(t_file_type f, long ts, const t_path& p, t_hash h = NULL_HASH) : 
     t_event::t_event(f, e_new, ts), path(p), hash(h)
   {}
 
@@ -191,15 +247,17 @@ struct t_event_new : public t_event {
 	     hash.c_str());   
     }
   }
+
 };
 
 struct t_event_delete : public t_event { 
   t_path path;
   t_hash hash;
 
-  t_event_delete(t_file_type f, long ts, const t_path& p, const t_hash& h) : 
-    t_event::t_event(f, e_delete, ts), path(p), hash(h)
-  {}
+  t_tree subtree;
+
+  t_event_delete(t_file_type f, long ts, const t_path& p, const t_hash& h = NULL_HASH) : 
+    t_event::t_event(f, e_delete, ts), path(p), hash(h), subtree() {}
 
   virtual void print() {
     printf("Deleted the %s \"%s\" in the folder \"%s\".\n",
@@ -207,6 +265,7 @@ struct t_event_delete : public t_event {
 	   get_name(path).c_str(), 
 	   path_to_string(get_parent(path)).c_str());
   }
+
 };
 
 struct t_event_modify : public t_event {
@@ -228,6 +287,7 @@ struct t_event_modify : public t_event {
 	   path_to_string(get_parent(path)).c_str(), 
 	   new_hash.c_str());
   }
+
 };
 
 struct t_event_move : public t_event {
@@ -292,6 +352,7 @@ struct t_event_copy : public t_event {
 	   path_to_string(get_parent(dest_path)).c_str(),
 	   get_name(dest_path).c_str());
   }
+
 };
 
 
@@ -305,9 +366,14 @@ class t_algo_state {
 
 public :
   t_algo_state () : hash_index(), events() {}
-  
-  t_hash_index hash_index;
+
   t_event_list events;
+
+  // Used to detect copy file events.
+  t_hash_index hash_index;
+
+  // Used to detect copy folder events.
+  t_tree_index tree_index;
 
 };
 
@@ -346,16 +412,27 @@ t_event_it remove_event (t_algo_state& state, t_event_it it) {
   std::cout << "REM - "; ev->print();
 
   state.events.erase(it);
+
   delete ev; // Should really be handled by boost::shared_ptr...
 
   return next_it;
 }
 
-void remove_event (t_algo_state& state, t_event_it start, t_event_it end) {
+t_event_it remove_event (t_algo_state& state, t_event_it start, t_event_it end) {
   t_event_it it = start;
   while (it != end) {
     it = remove_event(state, it);
   }
+  return it;
+}
+
+
+void print_tree (const t_tree& tree) {
+  std::cout << "TRE - ";
+  for (t_tree_cit it = tree.begin(); it != tree.end(); ++it) {
+    std::cout << "(" << it->first << ", " << it->second << ") ";
+  }
+  std::cout << std::endl;
 }
 
 
@@ -371,7 +448,6 @@ void print_index (t_algo_state& state) {
 void remove_from_index (t_algo_state& state, const t_hash& key, const t_path& value){
   const t_index_it start = state.hash_index.lower_bound(key);
   const t_index_it end = state.hash_index.upper_bound(key);
-  assert(start != state.hash_index.end());
 
   for (t_index_it it = start; it != end; it++) {
     if (it->second == value) {
@@ -449,13 +525,16 @@ bool simplify_to_copy_event(t_algo_state& state, p_event ev) {
   if (ev->event_type == e_new) {
     const t_event_new* new_ev = static_cast<t_event_new*> (ev);
 
-    t_index_it index_it = state.hash_index.upper_bound(new_ev->hash);
-    if (index_it != state.hash_index.end()) {
+    std::pair<t_index_it, t_index_it> match_range = 
+      state.hash_index.equal_range(new_ev->hash);
+
+    if (match_range.first != match_range.second) {
+      t_index_it index_it = match_range.first;
 
       p_event copy_ev = 
 	new t_event_copy(e_file, ev->timestamp, index_it->second, new_ev->path);
       state.events.insert(copy_ev);
-      std::cout << "COP - "; copy_ev->print();	  
+      std::cout << "COP - (" << index_it->first << ") "; copy_ev->print();	  
 
       return true;
     }   
@@ -519,10 +598,156 @@ void add_to_state (t_algo_state& state, p_event ev) {
 }
 
 
+bool simplify_folder_delete (t_algo_state& state, t_event_it prev_it, t_event_it cur_it) {
+  p_event prev_ev = *prev_it;
+  p_event cur_ev = *cur_it;
+
+  if (cur_ev->timestamp - prev_ev->timestamp > TIMESTAMP_TOLERANCE)
+    return false;
+  if (prev_ev->event_type != e_delete || cur_ev->event_type != e_delete)
+    return false;
+  if (cur_ev->file_type != e_folder)
+    return false;
+
+  t_event_delete* prev_del_ev = dynamic_cast<t_event_delete*>(prev_ev);
+  t_event_delete* cur_del_ev = dynamic_cast<t_event_delete*>(cur_ev);
+
+  if (cur_del_ev->path == get_parent(prev_del_ev->path)) {
+    t_tree& prev_subtree = prev_del_ev->subtree;
+    t_tree& cur_subtree = cur_del_ev->subtree;
+
+    std::string prev_name = get_name(prev_del_ev->path);
+    cur_subtree.insert(std::make_pair(prev_name, prev_del_ev->hash));
+
+    // Transfer the subtree entries into our own with our folder prefix.
+    for (t_tree_cit it = prev_subtree.begin(); it != prev_subtree.end(); ++it) {
+
+      std::string new_subpath = prev_name;
+      if (SEP[0] != it->first[0]) 
+	new_subpath += SEP;
+      new_subpath += it->first;
+      cur_subtree.insert(std::make_pair(new_subpath, it->second));
+    }
+    
+    remove_event(state, prev_it);
+    prev_it = state.events.end();
+    prev_ev = NULL;
+
+    return true;			       
+  }
+
+  return false;
+}
+
+
+// Finds a consecutive series of add events all within the same base folder.
+std::pair<t_event_it, t_tree>
+find_add_bounds (t_algo_state& state, t_event_it start, t_event_it end) {
+  
+  if (start == end || (*start)->event_type != e_new) 
+    return std::make_pair(start, t_tree());
+  
+  t_event_new* base_new_ev = static_cast<t_event_new*> (*start);
+  t_path prefix (base_new_ev->path.begin(), base_new_ev->path.end());
+
+  t_event_it ev_it = inc(start);
+
+  t_tree subtree;
+
+  for (; ev_it != end; ++ev_it) {
+    if ((*ev_it)->event_type != e_new)
+      break;
+    
+    t_event_new* cur_new_ev = static_cast<t_event_new*> (*ev_it);
+
+    // Do we have the same path prefix?
+    if (cur_new_ev->path.size() <= prefix.size())
+      break;
+    if (!std::equal(prefix.begin(), prefix.end(), cur_new_ev->path.begin()))
+      break;
+
+    t_path_it subpath_start = cur_new_ev->path.begin();
+    std::advance(subpath_start, base_new_ev->path.size());
+    std::string subpath = path_to_string(subpath_start, cur_new_ev->path.end());
+
+    subtree.insert(std::make_pair(subpath, cur_new_ev->hash));
+  }
+
+  return std::make_pair(ev_it, subtree);
+}
+
+
+t_event_it simplify_to_folder_move (t_algo_state& state, 
+				 t_event_it prev_it, 
+				 t_event_it cur_it)
+{
+  
+  if ((*prev_it)->event_type != e_delete || (*cur_it)->event_type != e_new)
+    return cur_it;
+
+  if ((*prev_it)->file_type != e_folder && (*prev_it)->file_type != e_folder)
+    cur_it;
+
+  t_event_delete* prev_del_ev = static_cast<t_event_delete*> (*prev_it);
+  t_event_new* base_new_ev = static_cast<t_event_new*> (*cur_it);
+
+  // Get the subtree.
+  std::pair<t_event_it, t_tree> bounds_result = 
+    find_add_bounds(state, cur_it, state.events.end());
+
+  t_event_it end_bound_it = bounds_result.first;
+  if (end_bound_it == cur_it)
+    return cur_it;
+  t_tree& add_subtree = bounds_result.second;
+
+  // Are the subtree the same?
+  if (add_subtree.size() != prev_del_ev->subtree.size())
+    return cur_it;
+  if (!std::equal(add_subtree.begin(), add_subtree.end(), prev_del_ev->subtree.begin()))
+    return cur_it;
+
+  // Ok, we have a move/rename event.
+  p_event move_ev = new t_event_move(e_folder, 
+				     base_new_ev->timestamp, 
+				     prev_del_ev->path,
+				     base_new_ev->path);
+
+  std::cout << "COP - "; move_ev->print();
+
+  remove_event(state, prev_it, end_bound_it);
+  return state.events.insert(move_ev);
+}
+
 
 
 void simplify_state (t_algo_state& state) {
 
+  std::cout << "SIM - Simplifying state." << std::endl;
+
+  t_event_it prev_it = state.events.begin();
+  t_event_it cur_it = inc(prev_it);
+
+  for (; cur_it != state.events.end(); ++cur_it, ++prev_it) {
+    p_event cur_ev = *cur_it;
+
+    while (simplify_folder_delete(state, prev_it, cur_it)) {
+      if (cur_it == state.events.begin())
+	break;
+      prev_it = dec(cur_it);
+    }
+
+    if (cur_it == state.events.begin()) 
+      continue;
+
+    cur_it = simplify_to_folder_move(state, prev_it, cur_it);
+
+    if (cur_it == state.events.begin())
+      continue;
+
+    prev_it = dec(cur_it);
+
+  }
+  
 }
 
 
@@ -538,7 +763,11 @@ void print_state (t_algo_state& state) {
 
 void run_tests() {
 
+  // File tests.
+
   {
+    std::cout << std::endl << " === TEST FILE ===" << std::endl;
+
     t_algo_state s;
     long ts = 0;
 
@@ -574,7 +803,36 @@ void run_tests() {
     add_to_state(s, new t_event_new(e_file, ++ts, make_path("/a/e/d.t"), "2223"));    
 
     simplify_state(s);
+    std::cout << std::endl;
+    print_state(s);
+  }
 
+  // Folder tests
+
+  {
+    std::cout << std::endl << std::endl << " === TEST FOLDER ===" << std::endl;
+
+    t_algo_state s;
+    long ts = 0;
+
+    // Delete folder tree.
+    add_to_state(s, new t_event_delete(e_file, ++ts, make_path("/a/b/c.t"), "1111"));
+    add_to_state(s, new t_event_delete(e_folder, ++ts, make_path("/a/b")));
+    add_to_state(s, new t_event_delete(e_file, ++ts, make_path("/a/d.t"), "2222"));
+    add_to_state(s, new t_event_delete(e_folder, ++ts, make_path("/a")));
+
+    // Move & rename folder /f to /g/h.
+    add_to_state(s, new t_event_delete(e_file, ++ts, make_path("/f/b/c.t"), "3333"));
+    add_to_state(s, new t_event_delete(e_folder, ++ts, make_path("/f/b")));
+    add_to_state(s, new t_event_delete(e_file, ++ts, make_path("/f/d.t"), "4444"));
+    add_to_state(s, new t_event_delete(e_folder, ++ts, make_path("/f")));
+
+    add_to_state(s, new t_event_new(e_folder, ++ts, make_path("/g/h")));
+    add_to_state(s, new t_event_new(e_file, ++ts, make_path("/g/h/d.t"), "4444"));
+    add_to_state(s, new t_event_new(e_folder, ++ts, make_path("/g/h/b")));
+    add_to_state(s, new t_event_new(e_file, ++ts, make_path("/g/h/b/c.t"), "3333"));
+
+    simplify_state(s);
     std::cout << std::endl;
     print_state(s);
   }
@@ -586,8 +844,6 @@ void run_tests() {
 void read_events (t_algo_state& state) {
   static const std::string ADD_EV = "ADD";
   static const std::string DEL_EV = "DEL";
-  static const std::string NULL_HASH = "-";
-
 
   int nb_events;
   std::cin >> nb_events;
